36b7ef8cc648ec9c7e8467861892aa4cbf6bd7cc
nakamura ryo <60230025+knr2@users.noreply.github.com>
2020-10-19 16:20:45 +0900

    update

--- crane_x7_examples/scripts/test1_pose_groupstate_example.py
+++ crane_x7_examples/scripts/test1_pose_groupstate_example.py
@@ -39,6 +39,59 @@ def main():
     arm.set_named_target("home")
     arm.go()
 
+#tuika
+    # アームを移動する
+    def move_arm(pos_x, pos_y, pos_z):
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = pos_x
+        target_pose.position.y = pos_y
+        target_pose.position.z = pos_z
+        q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
+
+    #ハンドを開く
+    move_gripper(1.3)
+    
+    
+    #掴む準備をする-----1
+    move_arm(0.23, 0.1, 0.2)
+    move_arm(0.15, 0.2, 0.15)
+
+    #掴みに行く
+    arm.set_max_velocity_scaling_factor(0.1)
+    move_arm(0.23, 0.2, 0.055)
+
+    #ハンドを閉じる
+    move_gripper(0.29)
+    move_max_velocity()
+    
+    #持ち上げる
+    move_arm(0.15, 0.2, 0.2)
+
+    #homeに戻る
+    arm.set_named_target("home")
+    arm.go()
+ 
+    #下ろす
+    move_arm(0.455, 0.0, 0.068)
+
+    #ハンドを開く
+    move_gripper(1.3)
+
+    #少しだけハンドを持ち上げる
+    move_arm(0.455, 0.0, 0.15)
+
+    #homeに戻る
+    arm.set_named_target("home")
+    arm.go()
+    
+#osimai
+
     # SRDFに定義されている"vertical"の姿勢にする
     print("vertical")
     arm.set_named_target("vertical")

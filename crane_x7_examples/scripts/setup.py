dcf23705939cade7f3ad444cb7b727d858662f92
nakamura ryo <60230025+knr2@users.noreply.github.com>
2020-11-02 15:59:26 +0900

    update

--- crane_x7_examples/scripts/setup.py
+++ crane_x7_examples/scripts/setup.py
@@ -1,89 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-引用元
-紙コップ　→　https://github.com/GakuKuwano/crane_x7_ros/blob/master/crane_x7_gazebo/worlds/table2.world
-アーム動作　→　https://github.com/GakuKuwano/crane_x7_ros/blob/master/crane_x7_examples/scripts/papercup_tower.py
-"""
-
-"""
-流れ
-準備
-    コップAとそれに対応するボールA、コップBとそれに対応するボールBを用意する
-    コップAとコップBは同y、ｚ座標に置く
-    コップA中にはボールAを入れておき、ボールBはコップBの横においておく
-
-        #ボールの確認
-
-    1.	コップAの手前に移動
-    2.	アーム先端を下に向ける
-    3.	アームを垂直に下げる
-    4.	コップAの下部を握る
-    5.	垂直に持ち上げる
-    6.	目標座標へ到着後1秒ほど停止
-    7.	コップAを下げる
-    8.	アームを全開に
-    9.	垂直に持ち上げる
-    10.	コップBの上に移動
-    11.	アーム先端を下に向ける
-    12.	アームを垂直に下げる
-    13.	コップBの上部を握る
-    14.	垂直に持ち上げる
-    15.	目標座標へ到着後1秒ほど停止
-    16.	横へ移動させボールBの上で停止
-    17.	垂直に降下させボールBにコップBをかぶせる
-    18.	コップBを横移動させ元の位置へ戻す
-    19.	アームを全開に
-    20.	垂直に持ち上げる
-
-        #パフォーマンス（消す前の）
-
-    1.	ホームへ戻る
-    2.	アームを全開に
-
-        #ボールを瞬間移動
-
-    1.	コップBの上に移動
-    2.	アーム先端を下に向ける
-    3.	アームを垂直に下げる
-    4.	コップBの下部を握る
-    5.	垂直に持ち上げる
-    6.	目標座標へ到着後1秒ほど停止
-    7.	コップBを下げる
-    8.	アームを全開に
-    9.	垂直に持ち上げる
-    10.	コップAの上に移動
-    11.	アーム先端を下に向ける
-    12.	アームを垂直に下げる
-    13.	コップAの上部を握る
-    14.	垂直に持ち上げる
-    15.	目標座標へ到着後1秒ほど停止
-    16.	コップAを下げ、アームを全開に
-    17.	垂直に持ち上げる
-
-
-        #ボールが瞬間移動したことを見せる
-
-    1.	コップAの上に移動
-    2.	コップAの上部を握る
-    3.	アーム先端を下に向ける
-    4.	垂直に持ち上げる
-    5.	コップAを手前に移動
-    6.	コップAを下す
-    7.	アームを全開に
-    8.	垂直に持ち上げる
-    9.	コップBの上に移動
-    10.	アーム先端を下に向ける
-    11.	コップBの下部を握る
-    12.	垂直に持ち上げる
-    13.	コップBを手前に移動
-    14.	コップBを下す
-    15.	アームを全開に
-    16.	垂直に持ち上げる
-    17.	ホームへ戻る
-"""
-
 import rospy
 import moveit_commander
 import geometry_msgs.msg
@@ -114,7 +31,7 @@ def main():
     ###
 
     #紙Aコップのx座標　紙Aコップのy座標　紙Bコップx座標 紙Bコップy座標
-    position_base =[[0.30, 0.13, 0.30, -0.11]]
+    position_base =[[0.29, 0.13, 0.29, -0.11]]
     Acup_tukamu = False
     Bcup_tukamu = False
 
@@ -122,15 +39,12 @@ def main():
     使い方
     ハンドをつかんだ時は
     position_manager(False,False,x,y,True)
-
     ハンドをはなした時は
     position_manager(False,False,x,y,False)
-
     Aカップをつかみたい時の手先の位置を知りたいときは
     aa = position_manager(True,True,0,0,False)
     x = aa[0]
     y = aa[1]
-
     BBカップをつかみたい時の手先の位置を知りたいときは
     aa = position_manager(True,False,0,0,False)
     x = aa[0]
@@ -247,7 +161,7 @@ def main():
         target_pose.position.x = pos_x - 0.13
         target_pose.position.y = pos_y
         target_pose.position.z = -0.01
-        q = quaternion_from_euler(-3.14/2.0 - 0.2, 3.14, -3.14/2.0)  # 上方から掴みに行く場合
+        q = quaternion_from_euler(-3.14/2.0 - 0.2, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
         target_pose.orientation.x = q[0]
         target_pose.orientation.y = q[1]
         target_pose.orientation.z = q[2]
@@ -261,7 +175,7 @@ def main():
         target_pose.position.x = pos_x - 0.005
         target_pose.position.y = pos_y
         target_pose.position.z = -0.01
-        q = quaternion_from_euler(-3.14/2.0 - 0.2, 3.14, -3.14/2.0)  # 上方から掴みに行く場合
+        q = quaternion_from_euler(-3.14/2.0 - 0.2, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
         target_pose.orientation.x = q[0]
         target_pose.orientation.y = q[1]
         target_pose.orientation.z = q[2]
@@ -289,7 +203,7 @@ def main():
         target_pose.position.x = pos_x - 0.005
         target_pose.position.y = pos_y
         target_pose.position.z = -0.01 + 0.1
-        q = quaternion_from_euler(-3.14/2.0, 3.14, -3.14/2.0)  # 上方から掴みに行く場合
+        q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
         target_pose.orientation.x = q[0]
         target_pose.orientation.y = q[1]
         target_pose.orientation.z = q[2]
@@ -308,8 +222,7 @@ def main():
     #ハンドを開く
     move_gripper(1.3)
     
-    #コップA下部を掴む-----1
-    #流れ　1,2,3,4,5,6,7,8,9
+    #コップAの初期位置
 
     #aaにコップAの(x,y)を代入
     aa = position_manager(True, True, 0, 0, False)
@@ -319,27 +232,18 @@ def main():
     move_arm_lower(aa[0], aa[1])
     #アームをコップの下部とくっつける
     move_arm_lower_catch(aa[0], aa[1])
-    #グリッパを閉じ、コップをつかむ
-    move_gripper(0.28)
-    #アームを持ち上げる(空中で停止させるため複数回呼び出している)
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    #アームを下げる
     move_arm_lower_catch(aa[0], aa[1])
-    #グリッパを開き、コップを放す
-    move_gripper(1.3)
+    move_arm_lower_catch(aa[0], aa[1])
+    move_arm_lower_catch(aa[0], aa[1])
+    move_arm_lower_catch(aa[0], aa[1])
     #アームを持ち上げコップから離れたらホームへ戻る
     move_arm_lower_up(aa[0], aa[1])
     move_max_velocity()
     arm.set_named_target("home")
     arm.go()
 
-    """
 
-    #コップB上部を掴む-----2
-    #流れ　10,11,12,13,14,15,16,17,18,19,20
+    #コップBの初期位置
 
     #aaにコップBの(x,y)を代入
     aa = position_manager(True, False, 0, 0, False)
@@ -349,169 +253,33 @@ def main():
     move_arm_upper(aa[0], aa[1])
     #アームをコップの下部とくっつける
     move_arm_upper_catch(aa[0], aa[1])
-    #グリッパを閉じ、コップをつかむ
-    move_gripper(0.28)
-    #アームを持ち上げる(空中で停止させるため複数回呼び出している)
+    move_arm_upper_catch(aa[0], aa[1])
+    move_arm_upper_catch(aa[0], aa[1])
+    move_arm_upper_catch(aa[0], aa[1])
+    move_arm_upper_catch(aa[0], aa[1])
+    #アームを持ち上げコップから離れたらホームへ戻る
     move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    #アームをボールの真上へ(空中で停止させるため複数回呼び出している)
-    move_arm_upper_up(aa[0], aa[1] + 0.1)
-    move_arm_upper_up(aa[0], aa[1] + 0.1)
-    move_arm_upper_up(aa[0], aa[1] + 0.1)
-    move_arm_upper_up(aa[0], aa[1] + 0.1)
-    #アームを下げカップをボールにかぶせる
+    move_max_velocity()
+    arm.set_named_target("home")
+    arm.go()
+
+    #ボールの初期位置
+
+    #aaにコップBの(x,y)を代入
+    aa = position_manager(True, False, 0, 0, False)
+    #グリッパを開く
+    move_gripper(1.3)
+    #アームをボールの位置に
     move_arm_upper_catch(aa[0], aa[1] + 0.1)
-    #元の位置へ戻す
-    move_arm_upper_catch(aa[0], aa[1])
-    #グリッパを開き、コップを放す
-    move_gripper(1.3)
+    move_arm_upper_catch(aa[0], aa[1] + 0.1)
+    move_arm_upper_catch(aa[0], aa[1] + 0.1)
+    move_arm_upper_catch(aa[0], aa[1] + 0.1)
+    move_arm_upper_catch(aa[0], aa[1] + 0.1)
     #アームを持ち上げコップから離れたらホームへ戻る
-    move_arm_upper_up(aa[0], aa[1])
+    move_arm_upper_up(aa[0], aa[1] + 0.1)
     move_max_velocity()
     arm.set_named_target("home")
     arm.go()
-
-
-    #パフォーマンスするならここ
-    #取り敢えず先輩のコードをそのまま流用
-
-    #頂点に振り上げる
-    move_arm(0.1, 0.0, 0.5)
-    #あおり
-    move_arm(0.22, -0.2, 0.2)
-    #頂点に振り上げる
-    move_arm(0.1, 0.0, 0.5)
-    #あおり
-    move_arm(0.22, 0.2, 0.2)
-    #homeに戻る
-    arm.set_named_target("home")
-    arm.go()
-
-
-    #コップB下部を掴む-----4
-    #瞬間移動　1,2,3,4,5,6,7,8,9
-
-    #aaにコップBの(x,y)を代入
-    aa = position_manager(True, False, 0, 0, False)
-    #グリッパを開く
-    move_gripper(1.3)
-    #アームをコップの下部と水平な部分へ移動
-    move_arm_lower(aa[0], aa[1])
-    #アームをコップの下部とくっつける
-    move_arm_lower_catch(aa[0], aa[1])
-    #グリッパを閉じ、コップをつかむ
-    move_gripper(0.28)
-    #アームを持ち上げる(空中で停止させるため複数回呼び出している)
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    #アームを下げる
-    move_arm_lower_catch(aa[0], aa[1])
-    #グリッパを開き、コップを放す
-    move_gripper(1.3)
-    #アームを持ち上げコップから離れたらホームへ戻る
-    move_arm_lower_up(aa[0], aa[1])
-    move_max_velocity()
-    arm.set_named_target("home")
-    arm.go()
-
-
-    #コップA上部を掴む-----4
-    #瞬間移動　10,11,12,13,14,15,16,17
-
-    #aaにコップAの(x,y)を代入
-    aa = position_manager(True, True, 0, 0, False)
-    #グリッパを開く
-    move_gripper(1.3)
-    #アームをコップの上部と水平な部分へ移動
-    move_arm_upper(aa[0], aa[1])
-    #アームをコップの上部とくっつける
-    move_arm_upper_catch(aa[0], aa[1])
-    #グリッパを閉じ、コップをつかむ
-    move_gripper(0.28)
-    move_arm_upper_up(aa[0], aa[1])
-    #アームを持ち上げる(空中で停止させるため複数回呼び出している)
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    #アームを下げる
-    move_arm_upper_catch(aa[0], aa[1])
-    #グリッパを開き、コップを放す
-    move_gripper(1.3)
-    #アームを持ち上げコップから離れたらホームへ戻る
-    move_arm_upper_up(aa[0], aa[1])
-    move_max_velocity()
-    arm.set_named_target("home")
-    arm.go()
-
-
-    #コップAの上部をつかむ-----5
-    #中身の確認
-
-    #aaにコップAの(x,y)を代入
-    aa = position_manager(True, True, 0, 0, False)
-    #グリッパを開く
-    move_gripper(1.3)
-    #アームをコップの上部と水平な部分へ移動
-    move_arm_upper(aa[0], aa[1])
-    #アームをコップの上部とくっつける
-    move_arm_upper_catch(aa[0], aa[1])
-    #グリッパを閉じ、コップをつかむ
-    move_gripper(0.28)
-    move_arm_upper_up(aa[0], aa[1])
-    #アームを持ち上げる(空中で停止させるため複数回呼び出している)
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    move_arm_upper_up(aa[0], aa[1])
-    #アームを手前に移動
-    move_arm_upper_up(aa[0] - 0.13, aa[1])
-    #アームを下げる
-    move_arm_upper(aa[0], aa[1])
-    #グリッパを開き、コップを放す
-    move_gripper(1.3)
-    #アームを持ち上げコップから離れたらホームへ戻る
-    move_arm_upper_up(aa[0], aa[1])
-    move_max_velocity()
-    arm.set_named_target("home")
-    arm.go()
-
-
-    #コップB下部を掴む-----6
-    #中身の確認
-
-    #aaにコップBの(x,y)を代入
-    aa = position_manager(True, False, 0, 0, False)
-    #グリッパを開く
-    move_gripper(1.3)
-    #アームをコップの下部と水平な部分へ移動
-    move_arm_lower(aa[0], aa[1])
-    #アームをコップの下部とくっつける
-    move_arm_lower_catch(aa[0], aa[1])
-    #グリッパを閉じ、コップをつかむ
-    move_gripper(0.28)
-    #アームを持ち上げる(空中で停止させるため複数回呼び出している)
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    move_arm_lower_up(aa[0], aa[1])
-    #アームを手前に移動
-    move_arm_lower_up(aa[0] - 0.13, aa[1])
-    #アームを下げる
-    move_arm_lower(aa[0], aa[1])
-    #グリッパを開き、コップを放す
-    move_gripper(1.3)
-    #アームを持ち上げコップから離れたらホームへ戻る
-    move_arm_lower_up(aa[0], aa[1])
-    move_max_velocity()
-    arm.set_named_target("home")
-    arm.go()
-
-    """
 
 
 if __name__ == '__main__':
